df80dcffd93205ff2ebe81aaa72cc2262be378ad
Oleksii Narinskyi <onarinskyi@apple.com>
2020-03-17 14:35:34 -0700

    Add test

---
+++ src/test/java/com/onarinskyi/MainTest.java
@@ -0,0 +1,14 @@
+package com.onarinskyi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void voidTest() throws Exception {
+        Thread.sleep(1000);
+        Assert.assertTrue(true);
+    }
+
+}
